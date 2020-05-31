a07446bf590a5e340128790844fcb85c84b45674
Wilson Huang <wilsonhuang995@gmail.com>
2020-05-31 04:23:42 -0700

    working binary search

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
-
-namespace Algorithms
+﻿namespace Algorithms
 {
+    using System;
+    using Algorithms.Searching;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var search = new Search();
+
+            int[] test = new int[] {1, 2, 3, 4, 5, 6, 7,8, 12, 14, 200, 400};
+            int ans = Search.BinarySearch<int>(test, -22);
+            Console.WriteLine(String.Format("{0}", ans));
+
         }
     }
 }
---
+++ Algorithms/Searching/BinarySearch.cs
@@ -0,0 +1,66 @@
+namespace Algorithms.Searching
+{
+    using System;
+    using System.Collections;
+
+    class Search
+    {
+        public Search() {
+            return;
+        }
+
+        public void Hi() {
+            Console.WriteLine("Hi from object");
+        }
+
+        /*
+         * Returns the index that val would belong in a sorted array arr
+         */
+        public static int BinarySearch<T> (T[] arr, T val) where T: IComparable 
+        {
+            int n = arr.Length;
+
+            if (n == 0) {
+                //val will not be in an empty array, and the correct position for inserting is index 0; hence, return -1;
+                return -1;
+            }
+
+            int low = 0; int high = n-1; int mid = (high + low) / 2;
+
+            while (high - low > 1) {
+                int comparison = val.CompareTo(arr[mid]);
+                if (comparison == 0) {
+                    return mid;
+                }
+
+                if (comparison > 0) 
+                {
+                    low=mid;
+                }
+                else 
+                {
+                    high = mid;
+                }
+
+                mid = (high + low) / 2;
+            }
+
+            if (val.CompareTo(arr[high]) == 0) return high; 
+            if (val.CompareTo(arr[low]) == 0) return low; 
+            
+            if (val.CompareTo(arr[high]) > 0) {
+                return -1 * high ;
+            }
+            if (val.CompareTo(arr[low]) < 0) {
+                //smaller than the entire array
+                return -1 * low - 1;
+            }
+            else 
+            {
+                //val is between higher and lower
+                return (-1 * high) - 1;
+            }
+            
+        }
+    }
+}
